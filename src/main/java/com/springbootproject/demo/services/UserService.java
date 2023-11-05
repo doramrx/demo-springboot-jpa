b72a84b43e4054d8602179216acd0878d2714f06
Isadora <isah3027@gmail.com>
2023-11-05 19:19:56 -0300

    Exception handling - update

--- src/main/java/com/springbootproject/demo/services/UserService.java
+++ src/main/java/com/springbootproject/demo/services/UserService.java
@@ -4,6 +4,7 @@ import com.springbootproject.demo.entities.User;
 import com.springbootproject.demo.repositories.UserRepository;
 import com.springbootproject.demo.services.exceptions.DatabaseException;
 import com.springbootproject.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -31,7 +32,6 @@ public class UserService {
     }
 
     public void delete(Long id){
-
         try {
             if(!repository.existsById(id)) throw new ResourceNotFoundException(id);
             repository.deleteById(id);
@@ -42,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
